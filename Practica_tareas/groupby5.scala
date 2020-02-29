5b014d49a1e6359213b97b5db69e579dbaf6a821
Aileen Enciso <60455757+aileenenciso@users.noreply.github.com>
2020-02-28 17:21:50 -0800

    Add files via upload
    Groupby 5

---
+++ Practica_tareas/groupby5.scala
@@ -0,0 +1,6 @@
+//// Enciso Maldonado Aileen Yurely 15210329
+// Garcia Bautista Ana Laura 15210793
+//Clase de caso Donut del Paso 3 y crear una secuencia de elementos donut de tipo Donut.
+println("\nStep 4: How to create a Sequence of type Donut")
+val donuts2: Seq[Donut] = Seq(Donut("Plain Donut", 1.5), Donut("Strawberry Donut", 2.0), Donut("Glazed Donut", 2.5))
+println(s"Elements of donuts2 = $donuts2")
