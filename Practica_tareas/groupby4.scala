c5c505b88880993703415edfb9200b1dabf771f2
Aileen Enciso <60455757+aileenenciso@users.noreply.github.com>
2020-02-28 17:20:58 -0800

    Add files via upload
    Groupby 4

---
+++ Practica_tareas/groupby4.scala
@@ -0,0 +1,5 @@
+// Enciso Maldonado Aileen Yurely 15210329
+// Garcia Bautista Ana Laura 15210793
+//clase de caso para representar objetos donut.
+println("\nStep 3: How to create a case class to represent Donut objects")
+case class Donut(name: String, price: Double)
